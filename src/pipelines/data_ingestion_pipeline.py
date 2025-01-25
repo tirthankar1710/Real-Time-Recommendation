8d926598cf7622c0ddd667f23cb0cc5d4810fc38
Tirthankar Ghosh <tirthankar@Tirthankars-Mac-mini.local>
2025-01-25 23:12:36 +0530

    major re-structuring for lambda functions

--- src/pipelines/data_ingestion_pipeline.py
+++ src/pipelines/data_ingestion_pipeline.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))) # For local
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))) # For local
 # sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) # For Lambda
 
 from src import logger
@@ -30,37 +30,37 @@ class DataIngestionTrainingPipeline:
         data_ingestion = DataIngestion(config=data_ingestion_config)
         data_ingestion.data_ingestion_flow(job_id=job_id)
 
-def lambda_handler(event, context):
-    """
-    AWS Lambda handler function.
-    Extracts the job ID from the event and initiates the data ingestion process.
-    """
-    try:
-        job_id = event.get("job_id")
-        if not job_id:
-            raise ValueError("Job ID is required!")
+# def lambda_handler(event, context):
+#     """
+#     AWS Lambda handler function.
+#     Extracts the job ID from the event and initiates the data ingestion process.
+#     """
+#     try:
+#         job_id = event.get("job_id")
+#         if not job_id:
+#             raise ValueError("Job ID is required!")
         
-        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = DataIngestionTrainingPipeline()
-        #TODO: Add the job_id as a parameter
-        obj.initiate_data_ingestion(job_id)
-        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-        return {
-                'statusCode': 200,
-                'body': {
-                    'message': f"Data ingestion completed for job ID: {job_id}",
-                    'job_id': job_id
-                }
-            }
-    except Exception as e:
-        logger.exception(e)
-        return {
-            'statusCode': 500,
-            'body': str(e)
-        }
+#         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+#         obj = DataIngestionTrainingPipeline()
+#         #TODO: Add the job_id as a parameter
+#         obj.initiate_data_ingestion(job_id)
+#         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+#         return {
+#                 'statusCode': 200,
+#                 'body': {
+#                     'message': f"Data ingestion completed for job ID: {job_id}",
+#                     'job_id': job_id
+#                 }
+#             }
+#     except Exception as e:
+#         logger.exception(e)
+#         return {
+#             'statusCode': 500,
+#             'body': str(e)
+#         }
 
-if __name__ == '__main__':
-    # For local testing
-    event = {'job_id': '123438954260589'}
-    context = {}
-    lambda_handler(event, context)
+# if __name__ == '__main__':
+#     # For local testing
+#     event = {'job_id': '123438954260589'}
+#     context = {}
+#     lambda_handler(event, context)
--- src/pipelines/model_trainer_pipeline.py
+++ src/pipelines/model_trainer_pipeline.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
 from src.config.configuration import ConfigurationManager
 from src.components.model_trainer import ModelTrainer
@@ -28,36 +28,36 @@ class ModelTrainerTrainingPipeline:
         model_trainer.model_trainer_flow(job_id)
 
 
-def lambda_handler(event, context):
-    """
-    AWS Lambda handler function.
-    Extracts the job ID from the event and initiates the data ingestion process.
-    """
-    try:
-        job_id = event.get("job_id")
-        if not job_id:
-            raise ValueError("Job ID is required!")
+# def lambda_handler(event, context):
+#     """
+#     AWS Lambda handler function.
+#     Extracts the job ID from the event and initiates the data ingestion process.
+#     """
+#     try:
+#         job_id = event.get("job_id")
+#         if not job_id:
+#             raise ValueError("Job ID is required!")
 
-        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = ModelTrainerTrainingPipeline()
-        obj.initiate_model_training(job_id)
-        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-        return {
-                'statusCode': 200,
-                'body': {
-                    'message': f"Data ingestion completed for job ID: {job_id}",
-                    'job_id': job_id
-                }
-            }
-    except Exception as e:
-        logger.exception(e)
-        return {
-            'statusCode': 500,
-            'body': str(e)
-        }
+#         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+#         obj = ModelTrainerTrainingPipeline()
+#         obj.initiate_model_training(job_id)
+#         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+#         return {
+#                 'statusCode': 200,
+#                 'body': {
+#                     'message': f"Data ingestion completed for job ID: {job_id}",
+#                     'job_id': job_id
+#                 }
+#             }
+#     except Exception as e:
+#         logger.exception(e)
+#         return {
+#             'statusCode': 500,
+#             'body': str(e)
+#         }
 
-if __name__ == '__main__':
-    # For local testing
-    event = {'job_id': 'e8ef154b-e333-46cf-b95f-6cc7343b34b9'}
-    context = {}
-    lambda_handler(event, context)
+# if __name__ == '__main__':
+#     # For local testing
+#     event = {'job_id': 'e8ef154b-e333-46cf-b95f-6cc7343b34b9'}
+#     context = {}
+#     lambda_handler(event, context)
